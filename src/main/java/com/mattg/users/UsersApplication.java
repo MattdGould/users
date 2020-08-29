31f6dc61de43e5aebd846a54e357d91604c02b27
Matthew Gould <matthew.gould@influentialsoftware.com>
2020-08-29 23:03:31 +0100

    Added logging on startup/shutdown

--- src/main/java/com/mattg/users/UsersApplication.java
+++ src/main/java/com/mattg/users/UsersApplication.java
@@ -1,13 +1,29 @@
 package com.mattg.users;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @SpringBootApplication
 public class UsersApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UsersApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
     }
 
+    @PostConstruct
+    public void startupApplication() {
+        LOGGER.info("Started application");
+    }
+
+    @PreDestroy
+    public void shutdownApplication() {
+        LOGGER.info("Application shutting down");
+    }
 }
